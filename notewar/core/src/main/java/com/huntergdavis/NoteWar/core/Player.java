f3c424f082fef42da6bbf8f5cef58128791f55a7
Hunter Davis <hunter@NoteWarDev.(none)>
2012-04-17 09:29:17 -0700

    added a player class and filled it with some methods

---
+++ notewar/core/src/main/java/com/huntergdavis/NoteWar/core/Player.java
@@ -0,0 +1,26 @@
+/*
+ * Player.java, implements Player class
+ *  
+ * Player has no knowledge of other players internally
+ * Player is simple abstraction
+ * Meant to be easily contained in LL or array etc 	
+ * 
+ */
+public class Player {
+	
+	
+	
+	/*
+	 * Player initialization
+	 * 
+	 */
+	Player() {
+	}
+ 
+	Player(int posx, int posy) {
+	}
+
+	Player(int posx, int posy, int health) {
+	}
+
+}

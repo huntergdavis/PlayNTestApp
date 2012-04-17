973bb7a48786f6f0756dfdff1a498f3410d0dc95
huntergdavis <hunter@hunterdavis.com>
2012-04-17 10:09:09 -0700

    created gameboard

---
+++ notewar/core/src/main/java/com/huntergdavis/NoteWar/core/GameBoard.java
@@ -0,0 +1,20 @@
+/*
+ * GameBoard.java, implements game board class
+ *  
+ * Base has no knowledge of other bases internally
+ * Base is simple abstraction
+ * Meant to be easily contained in LL or array etc 	
+ * 
+ */
+package com.huntergdavis.NoteWar.core;
+ 
+class Base extends GamePiece{
+	
+	/*
+	 * set the game piece type
+	 */
+	Base() {
+		Defaults();
+		gamePieceType = 1;
+	}
+}
